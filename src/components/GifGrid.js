3a539e6fe90a6461de0f7744cd0e91682f459838
David <jesus_limonhdz@hotmail.com>
2021-08-26 21:06:37 -0500

    Dcos agregado

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,24 +1,24 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import { getGifs } from '../helpers/getGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ( {category} ) => {
-    const [images, setImages] = useState([])
+    /* const [images, setImages] = useState([])
 
     useEffect( () =>{
         getGifs(category).then(setImages)
-    }, [category])
+    }, [category]) */
 
     
 
     return (
-        <div>
+        {/* <div>
             <h3> { category } </h3>
                 {
                     images.map( img => (
                         <GifGridItem key={img.id} {...img}/>
                     ))
                 }
-        </div>
+        </div> */}
     )
 }
